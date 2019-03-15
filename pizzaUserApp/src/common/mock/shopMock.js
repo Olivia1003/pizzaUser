a14090c415ec31ac54a2ecf19c9a7aad8d741d45
Yang <quangyang@unity3d.com>
2019-03-15 08:56:07 +0800

    add shop mock

---
+++ pizzaUserApp/src/common/mock/shopMock.js
@@ -0,0 +1,25 @@
+const shopMock = {
+    shop:
+         [
+            {
+                shopId: 1,
+                shopName: '麦当劳（金沙江路店）',
+                posX: '12。32131',
+                posY: '32.32131',
+                posString: '金沙江路3663号2312',
+                picUrl: 'http://ww.baidu.com',
+            },
+            {
+                shopId: 2,
+                shopName: '麦当劳（真北路店）',
+                posX: '12。32131',
+                posY: '32.32131',
+                posString: '金沙江路3663号2312',
+                picUrl: 'http://ww.baidu.com',
+            }
+        ]
+};
+
+module.exports = {
+    shopMock,
+};
